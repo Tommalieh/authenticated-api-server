164dbd7a2a07bda3ec8382d977c8a7f52e7bcbab
Tommalieh <muhammedtommalieh@gmail.com>
2020-06-17 21:55:46 +0300

    hotfix

--- __tests__/bearer.test.js
+++ __tests__/bearer.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const bearer = require('../src/middleware/bearer.js');
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImhlbGxvIiwiaWF0IjoxNTkxNzE0OTQ3fQ.IX0gYaRY84hlu_qMAQ4FMC6KJrZThCfngAouSsJJaEg';
+// const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImhlbGxvIiwiaWF0IjoxNTkxNzE0OTQ3fQ.IX0gYaRY84hlu_qMAQ4FMC6KJrZThCfngAouSsJJaEg';
 describe('bearer', () => {
   it('Invokes next() with a masseage invalid token authorization headers arent provided', () => {
     const req ={
